bd76a570d11ef1f9c996a9ebeaecfd47a259a353
David Ryder <davidryder@DavidMacBookPro.home>
2022-01-17 12:12:12 -0800

    Testing

--- kafka_producer.py
+++ kafka_producer.py
@@ -10,10 +10,10 @@ from confluent_kafka.admin import AdminClient, NewTopic, NewPartitions
 
 
 config = { "count":    int(os.environ.get("KAFKA_PRODUCER_COUNT", 604800)),
-           "interval": int( os.environ.get("KAFKA_PRODUCER_INTERVAL", 60)),
+           "interval": int(os.environ.get("KAFKA_PRODUCER_INTERVAL", 60)),
            "broker":   os.environ.get("KAFKA_BROKER", "kafka:9092"),
            "topic":    os.environ.get("KAFKA_TOPIC", "grafana"),
-           "partitions": os.environ.get("KAFKA_PARTITIONS", 4),
+           "partitions": int(os.environ.get("KAFKA_PARTITIONS", 4)),
  }
 
 dataWords = {
@@ -22,18 +22,6 @@ dataWords = {
     "processName": [ "ingester", "querier", "compactor", "distributor", "memcache", "gateway",
     "frontend"  ],
     "processName": [ "p1", "p2", "p3", "p4", "p5" ],
-}
-
-dataWords2 = {
-    "status": [ "running", "waiting", "stopped", "end" ],
-    "statusCode": [ 100, 200, 300, 400, 500, 600 ],
-    "processName": [ "ingester", "querier", "compactor", "distributor", "memcache", "gateway",
-    "frontend"  ],
-    "processName": [ "p1", "p2", "p3", "p4", "p5" ],
-    "location": [ { "latitude": 59.898300, "longitude": 30.261800, "city" :"St Petersburg", "city_geoname_id": 498817, "geoip_country_code": "LV"    },
-                  { "latitude": 52.375900, "longitude": 4.897500, "city": "Amsterdam", "city_geoname_id": 2759794, "geoip_country_code": "NL"  },
-                  { "latitude": 37.730900, "longitude": -122.388600, "city": "San Francisco","city_geoname_id": 5391959, "geoip_country_code": "US"    },
-                  { "latitude": 39.043700, "longitude": -77.487500, "city": "Ashburn", "city_geoname_id": 4744870, "geoip_country_code": "US"    } ]
 }
 
 def createLogLine(id=0, partition=0):
@@ -45,7 +33,7 @@ def createLogLine(id=0, partition=0):
         "id": id,
         "partition": partition
     }
-    if "location" in dataWords.keys():
+    if "xxxxlocation" in dataWords.keys():
         loc = random.choice(dataWords["location"])
         logLine.update( { "latitude": loc["latitude"], "longitude": loc["longitude"],
         "city": loc["city"],
@@ -62,36 +50,65 @@ def deliveryCallback(err, msg):
 
 kafkaConfig = {'bootstrap.servers': config["broker"]}
 
-try:
-    # Create topics
+def listTopics():
     ac = AdminClient(kafkaConfig)
-    print( "Topics: ", ac.list_topics( ) )
-    topicList = []
-    topicList.append( NewTopic(topic="grafana", num_partitions=config["partitions"], replication_factor=1) )
-    ac.create_topics(topicList )
-except Exception as e:
-    pritn( "Create topics {}".format(e))
+    topicList = ac.list_topics()
+    sys.stderr.write("List Topics: brokers: {} topics: {}\n".format(topicList.brokers, topicList.topics))
 
+def createKafkaTopicsWORKS():
+    try:
+        # Create topics
+        topic = config["topic"]
+        numPartitions = config["partitions"]
+        ac = AdminClient(kafkaConfig)
+        sys.stderr.write( "Topics: {}\n".format(ac.list_topics( )) )
+        topicList = []
+        topicList.append( NewTopic(topic=topic, num_partitions=numPartitions, replication_factor=1) )
+        ac.create_topics(topicList )
+        #sys.stderr.write("Created Topics: {} Partitions: {}\n".format(topic, numPartitions))
+        sys.stderr.write("Created Topics: {} Partitions: {} kconfig {}\n".format(topic, numPartitions, kafkaConfig))
+    except Exception as e:
+        sys.stderr.write( "Exception: Create topics {}\n".format(e))
+    listTopics()
+
+def createKafkaTopics():
+    try:
+        topic = config["topic"]
+        numPartitions = config["partitions"]
+        # Create topics
+        ac = AdminClient(kafkaConfig)
+        sys.stderr.write("Creating Topics: {} \n".format( ac.list_topics( ) ))
+        topicList = []
+        topicList.append( NewTopic(topic=topic, num_partitions=numPartitions, replication_factor=1) )
+        ac.create_topics(topicList )
+        sys.stderr.write("Created Topics: {} Partitions: {} kconfig {}\n".format(topic, numPartitions, kafkaConfig))
+    except Exception as e:
+        sys.stderr.write( "Exception: Create topics {}\n".format(e))
+    listTopics()
 
 #np = NewPartitions(topic="grafana", new_total_count=2)
 #ac.create_partitions(np)
 
+createKafkaTopicsWORKS()
+#createKafkaTopics()
+
 p1 = Producer(**kafkaConfig)
-print( "Producer: ", kafkaConfig, config["broker"], config["topic"])
+sys.stderr.write( "PRODUCER: kconfig: {} config: {} topic: {}\n".format(kafkaConfig, config["broker"], config["topic"]))
 
 partitionN = 0
 for i in range(0, config["count"]):
     #partition = random.randrange(0, config["partitions"])
-    partition = partitionN
-    partitionN = (partitionN + 1) % config["partitions"]
-    logMsg = json.dumps(createLogLine(id=i, partition=partition))
-    print( "{}: {} {}".format(i, logMsg, partition) )
+    logMsg = json.dumps(createLogLine(id=i, partition=partitionN))
+    listTopics()
+    sys.stderr.write( "PRODUCING: n: {}: msg: {} Partition: {}\n".format(i, logMsg, partitionN) )
     try:
         p1.produce(topic=config["topic"],
             value=logMsg,
-            partition=partition,
+            partition=partitionN,
             callback=deliveryCallback)
-    except BufferError:
-        sys.stderr.write("Kafka_producer: Queue Full: Topic: {} Messages: {}\n".format(config["topic"], len(p1)))
+    except Exception as e:
+        sys.stderr.write("Kafka_producer: Exception {}".format(e))
+        #sys.stderr.write("Kafka_producer: Queue Full: Topic: {} Messages: {}\n".format(config["topic"], len(p1)))
     p1.poll(0)
+    partitionN = (partitionN + 1) % config["partitions"]
     time.sleep(config["interval"])
